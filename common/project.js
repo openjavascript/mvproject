e0ac0f2f857324d85625978ed313b5b87ec1db65
qiushaowei <qiushaowei@360.cn>
2016-08-23 17:48:47 +0800

    up

--- common/project.js
+++ common/project.js
@@ -1,9 +1,9 @@
 
 module.exports = {
-    "default": "https://github.com/openjavascript/mvproject.project.webpack1.git"
-    , "webpack1": "https://github.com/openjavascript/mvproject.project.webpack1.git"
-    , "webpack2": "https://github.com/openjavascript/mvproject.project.webpack2.git"
-    , "webpack3": "https://github.com/openjavascript/mvproject.project.webpack3.git"
-    , "webpack4": "https://github.com/openjavascript/mvproject.project.webpack4.git"
-    , "webpack5": "https://github.com/openjavascript/mvproject.project.webpack5.git"
+    "default": "https://github.com/openjavascript/mvproject.project.webpack1/archive/master.zip"
+    , "webpack1": "https://github.com/openjavascript/mvproject.project.webpack1/archive/master.zip"
+    , "webpack2": "https://github.com/openjavascript/mvproject.project.webpack2/archive/master.zip"
+    , "webpack3": "https://github.com/openjavascript/mvproject.project.webpack3/archive/master.zip"
+    , "webpack4": "https://github.com/openjavascript/mvproject.project.webpack4/archive/master.zip"
+    , "webpack5": "https://github.com/openjavascript/mvproject.project.webpack5/archive/master.zip"
 };
